59b46420db3e480c81cbf2f562730c6461521812
nabeeljb <115611865+nabeeljb@users.noreply.github.com>
2023-07-04 13:22:06 +0530

    Update provisioner.sh

--- provisioner.sh
+++ provisioner.sh
@@ -4,14 +4,14 @@
 sudo yum -y update
 
 # install git
-sudo yum install git
+sudo yum install git -y
 
 # install SSM
-sudo yum install https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+sudo yum install https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm -y
 #sudo systemctl start amazon-ssm-agent
 
 # install cloudwatch agent
-sudo yum install https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
+sudo yum install https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm -y
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a start
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a status
 
